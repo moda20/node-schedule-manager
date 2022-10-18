43e9d96b4ce188ff89f9c9a03af4bf6276d355ba
Moda20 <kadhem03@gmail.com>
2022-10-18 20:01:41 +0100

    fix sql issues with log stats

--- Classes/Repositories/ScheduleJobLogRepository.js
+++ Classes/Repositories/ScheduleJobLogRepository.js
@@ -80,13 +80,13 @@ class ScheduleJobLogRepository {
       let sqlData = [];
       const jobIds = Array.isArray(jobId) ? jobId : [jobId];
 
-      let sql = `SELECT job_id as id, AVG(start_time - end_time) as avgTime, MAX(start_time) as latestStart, MAX(end_time) as LatestEnds from schedule_job_log group by job_id ${jobId ? `where job_id IN ?`: ''}`;
-      sqlData.push(`(${jobIds.map(e=>"\'"+e+"\'").join(',')})`);
+      let sql = `SELECT job_id as id, AVG(start_time - end_time) as avgTime, MAX(start_time) as latestStart, MAX(end_time) as LatestEnds from schedule_job_log ${jobId ? `where job_id IN (${jobIds.map(e=>"\'"+e+"\'").join(',')})`: ''} group by job_id`;
+
       if(sql === '') {
         return {success:false, err: 'get log failed'};
       }
 
-      let result = await MySQL.query(sql, sqlData, {selectQuery: true});
+      let result = await MySQL.query(sql, undefined, {selectQuery: true});
 
       return {success:true, result:result};
 
