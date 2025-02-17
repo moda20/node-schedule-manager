216504081bd833d0f0e66dc992bd45abdae6c82e
Moda20 <kadhem03@gmail.com>
2025-02-18 00:07:01 +0100

    fix SQL table creation query

--- init_sql.js
+++ init_sql.js
@@ -10,7 +10,7 @@ module.exports = {
     '`exclusive` varchar(5) NOT NULL DEFAULT \'\', ' +
     '`status` varchar(10) NOT NULL DEFAULT \'\', ' +
     '`average_time` float NOT NULL DEFAULT \'\', ' +
-    '`create_at` DATETIME DEFAULT CURRENT_TIMESTAMP null, \'\', ' +
+    '`create_at` DATETIME DEFAULT CURRENT_TIMESTAMP null \'\', ' +
     'PRIMARY KEY (`job_id`), ' +
     'UNIQUE KEY `job_name` (`job_name`) ' +
   ') ENGINE=InnoDB DEFAULT CHARSET=utf8',
